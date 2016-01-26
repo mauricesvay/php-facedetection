753868082a971b854cc3d2ed3a8c95eb8e3d9fa8
Alexandr Sorochkin <sorochkin@topface.com>
2016-01-26 13:15:15 +0300

    added cropFaceToJpeg method

---
+++ Exception/NoFaceException.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Throws exception if face was not detected in `faceDetect` call.
+ */
+
+namespace svay\Exception;
+
+use Exception;
+
+class NoFaceException extends Exception {
+
+}
